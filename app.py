37a4d102003945ef3e2eb89dfbe358948dc2783e
Zyashx07 <rakeshop009@gmail.com>
2025-10-29 16:30:09 +0530

    Small changes to backend code

--- app.py
+++ app.py
@@ -319,4 +319,4 @@ def logout():
 
 # -------------------- RUN --------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
